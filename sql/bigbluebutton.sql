341079b276f4c5d201957cce19ceb1579bee4419
CD48LOZERE <75454240+CD48LOZERE@users.noreply.github.com>
2020-12-16 17:14:23 +0100

    Update bigbluebutton.sql

--- sql/bigbluebutton.sql
+++ sql/bigbluebutton.sql
@@ -3,6 +3,7 @@ CREATE TABLE `meetings` (
   `meetingID` varchar(40) COLLATE utf8mb4_bin NOT NULL,
   `attendeePW` varchar(50) COLLATE utf8mb4_bin NOT NULL,
   `moderatorPW` varchar(50) COLLATE utf8mb4_bin NOT NULL,
+  `voiceBridge` varchar(50) COLLATE utf8mb4_bin NOT NULL,
   `created` timestamp NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
